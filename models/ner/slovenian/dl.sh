fe70a720fdddf9b7bec3f49f7104ceeb1a1015d6
asahtik <asahtik@gmail.com>
2023-03-16 20:46:04 +0100

    Added links to ner models

---
+++ models/ner/slovenian/dl.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+curl --remote-name-all https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1321{/ssj500k,/ssj500k.pretrain.pt}
+curl --remote-name-all https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1339{/sl_nstd}
